3a65535cb3d1cc6d4422959f25ffef56a3e52a9e
theunknowninfinite <68436564+theunknowninfinite@users.noreply.github.com>
2022-11-09 13:47:15 -0500

    Bringup and changes for my profile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,17 +11,16 @@ const Footer = (props) => {
         <i className="fas fa-code" /> with <i className="fas fa-heart" /> by{" "}
         <a
           rel="noopener"
-          href="https://github.com/hashirshoaeb"
+          href="https://github.com/theunknowninfinite"
           aria-label="My GitHub"
         > <span className="badge bg-dark">
-            Hashir Shoaib
+            Suriya Suresh
           </span>
         </a>{" "}
         using <i className="fab fa-react" />
         <p>
           <small className="text-muted">
-            Project code is open source. Feel free to fork and make your own
-            version.
+            Project code is open source. Forked from https://github.com/hashirshoaeb.
           </small>
         </p>
       </Container>
